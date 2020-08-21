ef20920b6100279806070b766a45d12eb5f07314
Albert Yang <albertesyang@gmail.com>
2020-08-21 16:09:25 -0400

    more conflicts

--- src/components/Giphy.js
+++ src/components/Giphy.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import '../styles/Giphy.scss';
 import axios from 'axios';
@@ -24,38 +23,4 @@ const Giphy = (props) => {
         </div> : null
     )
 }
-=======
-import React from 'react';
-import '../styles/Giphy.scss';
-import axios from 'axios';
-//variable key word, try with hard code first then add the weather connect 
-class Giphy extends React.Component{
-    state={
-        weathergif:null
-    }
-        componentDidMount(){
-        const { keyword }= this.props;
-        if (keyword){
-            axios.get(`http://api.giphy.com/v1/gifs/translate?s=${keyword.split('_').join(' ')}&api_key=xgQ2r5ca2ohkkDNIzYD1nOgji38B7g4b`)
-                .then(res=> {
-                    this.setState({
-                        weathergif:`https://media.giphy.com/media/${res.data.data.id}/giphy.gif`
-                       
-                    })
-                })
-            }
-        }
-
-        render(){
-            if(!this.state.weathergif){
-                return null
-            }
-            return(
-                <div className="gif-container">
-                    <img className="gif" src={this.state.weathergif}/>  
-                </div>
-            )
-        }
-    }
->>>>>>> 407e948... last commit
 export default Giphy;
