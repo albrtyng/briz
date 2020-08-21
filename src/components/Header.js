31c6efc86ffbabc4dcbb9192be5a2aeb0003c9d9
katherine allen <katherineallen067@gmail.com>
2020-08-21 15:37:36 -0400

    last commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,17 @@ import briz from '../assets/images/wind-cartoon.png';
 class Header extends React.Component{
     render(){
         return(
+<<<<<<< HEAD
           <div className="header">
               <img className="header-logo" src={briz} alt="briz logo"/>
               <h1 className="logo">brīz</h1>
           </div>
+=======
+                <div className="header">
+                    <img className="header-logo" src={briz} alt="briz logo"/>
+                    <h1 className="logo">brīz</h1>
+                </div>
+>>>>>>> 407e948... last commit
         )
     }
 }
